dd57b780e4e099613b42cbf094ee95a8030d26db
= <shivampathak19972@gmail.com>
2025-12-31 23:35:36 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,23 @@ class App extends Component {
       percent: 0,
       width: 0
     };
+
     this.widths = [0, 12.5, 25, 37.5, 50, 62.5, 75, 87.5, 100, 112.5, 125, 137.5, 150];
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
       this.setState((prev) => {
-        if (prev.step >= 10) {
+        if (prev.step >= 10) { 
           clearInterval(this.interval);
           return prev;
         }
+
         const nextStep = prev.step + 1;
         return {
           step: nextStep,
           percent: nextStep * 10,
-          width: this.widths[nextStep]
+          width: this.widths[nextStep] 
         };
       });
     }, 1000);
