2740cf0490178c462c34b32889db1c6bffb9713b
= <shivampathak19972@gmail.com>
2025-12-31 23:31:57 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
-import "./../styles/App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      step: 0,
       percent: 0,
       width: 0
     };
+    this.widths = [0, 12.5, 25, 37.5, 50, 62.5, 75, 87.5, 100, 112.5, 125, 137.5, 150];
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
       this.setState((prev) => {
-        if (prev.percent >= 100) {
+        if (prev.step >= 10) {
           clearInterval(this.interval);
           return prev;
         }
-
+        const nextStep = prev.step + 1;
         return {
-          percent: prev.percent + 10,
-          width: prev.width + 12.5
+          step: nextStep,
+          percent: nextStep * 10,
+          width: this.widths[nextStep]
         };
       });
     }, 1000);
@@ -33,14 +35,12 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* Progress text */}
         <p>{this.state.percent}%</p>
 
-        {/* Progress bar */}
         <div
           id="barOuter"
           style={{
-            width: "125px",  
+            width: "150px",
             height: "20px",
             border: "1px solid black"
           }}
